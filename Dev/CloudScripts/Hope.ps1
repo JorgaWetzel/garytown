341c03ef1aac07fe07e8b20547742f48ec878eb7
JorgaWetzel <51820881+JorgaWetzel@users.noreply.github.com>
2024-03-14 17:12:24 +0100

    Update Hope.ps1

--- Dev/CloudScripts/Hope.ps1
+++ Dev/CloudScripts/Hope.ps1
@@ -4,7 +4,7 @@ Creates Setup Complete Files
 #>
 
 $ScriptName = 'hope.garytown.com'
-$ScriptVersion = '24.1.22.1'
+$ScriptVersion = '14.03.2024'
 
 iex (irm raw.githubusercontent.com/JorgaWetzel/garytown/master/Dev/CloudScripts/Functions.ps1)
 #region functions
