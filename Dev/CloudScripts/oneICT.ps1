a22565e5bb7381ca30044967006f631b819e2d28
JorgaWetzel <51820881+JorgaWetzel@users.noreply.github.com>
2024-03-22 15:09:36 +0100

    Update oneICT.ps1

--- Dev/CloudScripts/oneICT.ps1
+++ Dev/CloudScripts/oneICT.ps1
@@ -9,80 +9,6 @@ $ScriptVersion = '14.03.2024'
 iex (irm raw.githubusercontent.com/JorgaWetzel/garytown/master/Dev/CloudScripts/Functions.ps1)
 #region functions
 
-function Use-WindowsUnattend.specialize {
-    [CmdletBinding()]
-    param (
-        [string]$AutopilotSwitches
-    )
-#https://docs.microsoft.com/en-us/windows-hardware/customize/desktop/unattend/microsoft-windows-deployment-runsynchronous
-$UnattendXml = @'
-<?xml version="1.0" encoding="utf-8"?>
-<unattend xmlns="urn:schemas-microsoft-com:unattend">
-<settings pass="specialize">
-<component name="Microsoft-Windows-IE-InternetExplorer" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-    <LocalIntranetSites>*.internal.loc</LocalIntranetSites>
-    <Home_Page>https://www.google.ch</Home_Page>
-    <DisableFirstRunWizard>true</DisableFirstRunWizard>
-</component>
-<component name="Microsoft-Windows-Shell-Setup" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-    <TimeZone>W. Europe Standard Time</TimeZone>
-    <RegisteredOrganization></RegisteredOrganization>
-    <RegisteredOwner>Administrator</RegisteredOwner>
-</component>
-<component name="Microsoft-Windows-International-Core" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-    <InputLocale>0807:00000807</InputLocale>
-    <SystemLocale>en-US</SystemLocale>
-    <UserLocale>de-CH</UserLocale>
-</component>
-</settings>
-</unattend>
-'@
-    #=================================================
-    #	Block
-    #=================================================
-    #Block-WinOS
-    #Block-WindowsVersionNe10
-    #Block-PowerShellVersionLt5
-    #=================================================
-    #   Specialize.cmd
-    #=================================================
-    Write-Verbose "Adding C:\Windows\Setup\Scripts\Specialize.cmd"
-
-    if (-NOT (Test-Path 'C:\Windows\Setup\Scripts')) {
-        New-Item -Path 'C:\Windows\Setup\Scripts' -ItemType Directory -Force | Out-Null
-    }
-
-$Specialize = @'
-
-'@
-
-    $Specialize | Out-File -FilePath 'C:\Windows\Setup\Scripts\Specialize.cmd' -Encoding ascii -Width 2000 -Force
-    #=================================================
-    #	Panther Unattend.xml
-    #=================================================
-    $Panther = 'C:\Windows\Panther'
-    $UnattendPath = "$Panther\Specialize.xml"
-
-    if (-NOT (Test-Path $Panther)) {
-        New-Item -Path $Panther -ItemType Directory -Force | Out-Null
-    }
-
-    Write-Verbose -Verbose "Setting Specialize.xml at $UnattendPath"
-    $UnattendXml | Out-File -FilePath $UnattendPath -Encoding utf8 -Width 2000 -Force
-    #=================================================
-    #	Use-WindowsUnattend
-    #=================================================
-    Write-Verbose -Verbose "Use-WindowsUnattend -Path 'C:\' -UnattendPath $UnattendPath"
-    Use-WindowsUnattend -Path 'C:\' -UnattendPath $UnattendPath -Verbose
-    
-    #Write-Verbose -Verbose "Copy-PSModuleToFolder -Name OSD to C:\Program Files\WindowsPowerShell\Modules"
-    #Copy-PSModuleToFolder -Name OSD -Destination 'C:\Program Files\WindowsPowerShell\Modules'
-
-    # Notepad 'C:\Windows\Setup\Scripts\Specialize.cmd'
-    #=================================================
-}
-
-
 Write-Host -ForegroundColor Green "[+] $ScriptName $ScriptVersion ($WindowsPhase Phase)"
 
 Set-ExecutionPolicy Bypass -Force
@@ -133,8 +59,6 @@ if ($transcriptLine -ne $null -and $restartLine -ne $null) {
     #Add-Content -Path $PSFilePath "Write-Output 'Weiterer neuer Inhalt'"
     Add-Content -Path $PSFilePath "Stop-Transcript"
     Add-Content -Path $PSFilePath "Restart-Computer -Force"
-
-    Use-WindowsUnattend.specialize
 }    
     # restart-computer
 }
