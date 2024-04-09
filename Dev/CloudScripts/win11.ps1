fc1ea99b5be04a979bc29de7f770abafaea95bc2
JorgaWetzel <51820881+JorgaWetzel@users.noreply.github.com>
2024-04-09 18:19:19 +0200

    Update win11.ps1

--- Dev/CloudScripts/win11.ps1
+++ Dev/CloudScripts/win11.ps1
@@ -91,8 +91,8 @@ Write-Host "Starting OSDCloud" -ForegroundColor Green
 #write-host "Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage"
 
 # Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage
-# Start-OSDCloudGUI
-Start-OSDCloudGUIDev
+Start-OSDCloudGUI
+# Start-OSDCloudGUIDev
 
 write-host "OSDCloud Process Complete, Running Custom Actions From Script Before Reboot" -ForegroundColor Green
 
