1c816cc7aff10deaf16f2f4f3f9980c56737d4c8
JorgaWetzel <51820881+JorgaWetzel@users.noreply.github.com>
2024-04-10 17:47:18 +0200

    Update Functions2.ps1

--- Dev/CloudScripts/Functions2.ps1
+++ Dev/CloudScripts/Functions2.ps1
@@ -19,28 +19,14 @@ function Set-DefaultProfilePersonalPrefOneICT {
     }
     reg load $VirtualRegistryPath_defaultuser $REG_defaultuser | Out-Null
 
-    #TaskBar Left / Hide Chat / Hide Widgets / Hide TaskView
-    $Path = "$VirtualRegistryPath_software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
-    New-ItemProperty -Path $Path -Name "TaskbarAl" -Value 0 -PropertyType Dword -Force | Out-Null
-    New-ItemProperty -Path $Path -Name "TaskbarMn" -Value 0 -PropertyType Dword -Force | Out-Null
-    New-ItemProperty -Path $Path -Name "TaskbarDa" -Value 0 -PropertyType Dword -Force | Out-Null
-    New-ItemProperty -Path $Path -Name "ShowTaskViewButton" -Value 0 -PropertyType Dword -Force | Out-Null
+    # Disabling Bing Search in Start Menu, Disabling Cortana
+    $Path = "$VirtualRegistryPath_software\Microsoft\Windows\CurrentVersion\Search"
+    Set-ItemProperty -Path $Path -Name "BingSearchEnabled" -Type DWord -Value 0
+    Set-ItemProperty -Path $Path -Name "CortanaConsent" -Type DWord -Value 0
+    If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search")) {
+        New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Force | Out-Null
+    }
 
-    #Disable Content Delivery
-    $Path = "$VirtualRegistryPath_software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager"
-    New-ItemProperty -Path $Path -Name "SystemPaneSuggestionsEnabled" -Value 0 -PropertyType Dword -Force | Out-Null
-    New-ItemProperty -Path $Path -Name "SubscribedContentEnabled" -Value 0 -PropertyType Dword -Force | Out-Null
-    New-ItemProperty -Path $Path -Name "SoftLandingEnabled" -Value 0 -PropertyType Dword -Force | Out-Null
-    New-ItemProperty -Path $Path -Name "SilentInstalledAppsEnabled" -Value 0 -PropertyType Dword -Force | Out-Null
-    New-ItemProperty -Path $Path -Name "PreInstalledAppsEnabled" -Value 0 -PropertyType Dword -Force | Out-Null
-    New-ItemProperty -Path $Path -Name "OemPreInstalledAppsEnabled" -Value 0 -PropertyType Dword -Force | Out-Null
-    New-ItemProperty -Path $Path -Name "FeatureManagementEnabled" -Value 0 -PropertyType Dword -Force | Out-Null
-    New-ItemProperty -Path $Path -Name "ContentDeliveryAllowed" -Value 0 -PropertyType Dword -Force | Out-Null
-
-    #Enable Location for Auto Time Zone
-    $Path = "$VirtualRegistryPath_software\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\location"
-    New-Item -Path $Path -ItemType Directory -Force | Out-Null
-    New-ItemProperty -Path $Path -Name "Value" -Value Allow -PropertyType String -Force | Out-Null
     Start-Sleep -s 1
     reg unload $VirtualRegistryPath_defaultuser | Out-Null
 }
