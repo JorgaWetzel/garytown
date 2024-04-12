fafa4fbd730a1eb0d45725f7575322c92f34c8df
JorgaWetzel <51820881+JorgaWetzel@users.noreply.github.com>
2024-04-12 15:52:32 +0200

    Update oneICT.ps1

--- Dev/CloudScripts/oneICT.ps1
+++ Dev/CloudScripts/oneICT.ps1
@@ -99,7 +99,8 @@ if ($env:SystemDrive -ne 'X:') {
     
     Write-Host -ForegroundColor Gray "**Setting Default Profile Personal Preferences**" 
     Set-DefaultProfilePersonalPref
-    # Set-DefaultProfilePersonalPrefOneICT
+    Set-DefaultProfilePersonalPrefOneICT
+    
     
     #Try to prevent crap from auto installing
     Write-Host -ForegroundColor Gray "**Disabling Cloud Content**" 
