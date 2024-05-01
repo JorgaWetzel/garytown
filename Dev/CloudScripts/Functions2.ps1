a928a82fce24d5d64a98d8f1a2988330f7390905
JorgaWetzel <51820881+JorgaWetzel@users.noreply.github.com>
2024-05-01 11:09:37 +0200

    Update Functions2.ps1

--- Dev/CloudScripts/Functions2.ps1
+++ Dev/CloudScripts/Functions2.ps1
@@ -761,6 +761,19 @@ function Set-RunOnceScript {
     $certStore.Close()
     Remove-Item -Path $tempPath
     Write-Host "Das Zertifikat wurde erfolgreich zu TrustedPeople unter LocalMachine hinzugefuegt."
+
+    # Scheduled Task for "choco upgrade -y"
+    $schtaskName = "Chocolatey Upgrade All"
+    $schtaskDescription = "Upgade der mit Chocolaty verwalteten Paketen. V$($Version)"
+    $trigger1 = New-ScheduledTaskTrigger -AtStartup
+    $trigger2 = New-ScheduledTaskTrigger -Weekly -WeeksInterval 1 -DaysOfWeek Wednesday -At 8pm
+    $principal= New-ScheduledTaskPrincipal -UserId 'SYSTEM'
+    $action = New-ScheduledTaskAction -Execute "C:\ProgramData\chocolatey\choco.exe" -Argument 'upgrade all -y'
+    $settings= New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable
+
+Register-ScheduledTask -TaskName $schtaskName -Trigger $trigger1,$trigger2 -Action $action -Principal $principal -Settings $settings -Description $schtaskDescription -Force
+
+
     }
 
     Write-Host -ForegroundColor Green "[+] Function DisableIPv6"
