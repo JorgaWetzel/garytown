5c092b00247a83ff94654ac3e929d0f5bd71c487
JorgaWetzel <51820881+JorgaWetzel@users.noreply.github.com>
2024-05-23 17:38:11 +0200

    Update oneICT.ps1

--- Dev/CloudScripts/oneICT.ps1
+++ Dev/CloudScripts/oneICT.ps1
@@ -82,13 +82,6 @@ if ($env:SystemDrive -ne 'X:') {
         iex (irm https://raw.githubusercontent.com/suazione/CodeDump/main/Set-ConfigureChatAutoInstall.ps1)
     }
     catch {}
-    # Add Hope PDF to Desktop
-    Write-Host -ForegroundColor Gray "**Adding HOPE PDF to Desktop**" 
-    try {
-        Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/gwblok/garytown/85ad154fa2964ea4757a458dc5c91aea5bf483c6/HopeForUsedComputers/Hope%20for%20Used%20Computers%20PDF.pdf" -OutFile "C:\Users\Public\Desktop\Hope For Used Computers.pdf"
-    }
-    catch {}
-
     #Set DO
     #Set-DOPoliciesGPORegistry
     
