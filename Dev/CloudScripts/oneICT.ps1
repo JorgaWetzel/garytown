1256d5f2f7d8c1b01a6295765a5c582ece1801ed
JorgaWetzel <51820881+JorgaWetzel@users.noreply.github.com>
2024-05-24 17:07:04 +0200

    Update oneICT.ps1

--- Dev/CloudScripts/oneICT.ps1
+++ Dev/CloudScripts/oneICT.ps1
@@ -80,7 +80,7 @@ if ($transcriptLine -ne $null -and $restartLine -ne $null) {
     Add-Content -Path $PSFilePath "Stop-Transcript"
     Add-Content -Path $PSFilePath "Restart-Computer -Force"
 }    
-    # restart-computer
+    restart-computer
 
 }
 
