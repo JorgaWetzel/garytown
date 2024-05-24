eb80d87ee0c1d8b395292568af4c4f509138f938
JorgaWetzel <51820881+JorgaWetzel@users.noreply.github.com>
2024-05-24 11:57:07 +0200

    Update oneICT.ps1

--- Dev/CloudScripts/oneICT.ps1
+++ Dev/CloudScripts/oneICT.ps1
@@ -28,15 +28,7 @@ else {
 if ($WindowsPhase -eq 'WinPE') {
     Write-Host -ForegroundColor Green "Starting win11.oneict.ch"
     iex (irm https://raw.githubusercontent.com/JorgaWetzel/garytown/master/Dev/CloudScripts/win11.ps1)
-    
-    #Create Unattend.xml
-    <#
-    $url = "https://raw.githubusercontent.com/JorgaWetzel/garytown/master/Dev/CloudScripts/Unattend.xml"
-    $destinationPath = "C:\Windows\Panther\unattend.xml"
-    Invoke-WebRequest -Uri $url -OutFile $destinationPath
-    # Notepad $destinationPath
-    #>
-    
+        
     #Create Custom SetupComplete
     $ScriptsPath = "C:\Windows\Setup\Scripts"
     $PSFilePath = "$ScriptsPath\SetupComplete.ps1"
