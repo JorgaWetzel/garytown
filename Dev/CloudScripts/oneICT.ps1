87270520264249b00c5f11c0515cde15e0e4c5fb
JorgaWetzel <51820881+JorgaWetzel@users.noreply.github.com>
2024-06-04 11:03:46 +0200

    Update oneICT.ps1

--- Dev/CloudScripts/oneICT.ps1
+++ Dev/CloudScripts/oneICT.ps1
@@ -164,14 +164,14 @@ if ($env:SystemDrive -ne 'X:') {
     # Set-TimeZoneFromIP
     
     #Set OOBE Language
-    <#
+    
     Set-WinUILanguageOverride -Language de-CH
     Set-WinCultureFromLanguageListOptOut -OptOut $false
     Set-Culture -CultureInfo de-CH
     $InputMethod = '0807:00000807' # Das Layout für Deutsch (Schweiz)
     Set-WinUserLanguageList -LanguageList (New-WinUserLanguageList $InputMethod) -Force
     Set-WinSystemLocale -SystemLocale de-CH
-    #>
+    
     
     # Setup oneICT Chocolatey Framework
     Write-Host -ForegroundColor Gray "**Running Chocolatey Framework**"
