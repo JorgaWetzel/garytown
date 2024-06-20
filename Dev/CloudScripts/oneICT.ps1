0a8057cd27c89163f196b6c4544a29bda1670bf6
JorgaWetzel <51820881+JorgaWetzel@users.noreply.github.com>
2024-06-20 10:22:07 +0200

    Update oneICT.ps1

--- Dev/CloudScripts/oneICT.ps1
+++ Dev/CloudScripts/oneICT.ps1
@@ -97,8 +97,8 @@ if ($env:SystemDrive -ne 'X:') {
     Set-Win11ReqBypassRegValues
 
     # setup RunOnce to execute provisioning.ps1 script
-    Write-Host -ForegroundColor Gray "**Running Set-RunOnceScript Script**"
-    Set-RunOnceScript
+    # Write-Host -ForegroundColor Gray "**Running Set-RunOnceScript Script**"
+    # Set-RunOnceScript
    
     #Windows Updates
     #Write-Host -ForegroundColor Gray "**Running Defender Updates**"
