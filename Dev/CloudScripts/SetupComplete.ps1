a764db5c87b1f46a6789b05652fb9517b5ad48aa
JorgaWetzel <51820881+JorgaWetzel@users.noreply.github.com>
2024-08-07 10:22:01 +0200

    Update SetupComplete.ps1

--- Dev/CloudScripts/SetupComplete.ps1
+++ Dev/CloudScripts/SetupComplete.ps1
@@ -329,15 +329,15 @@ function Step-oobeStopComputer {
 
 # Execute functions
 Step-KeyboardLanguage
-Step-oobeExecutionPolicy
-Step-oobePackageManagement
-Step-oobeTrustPSGallery
+# Step-oobeExecutionPolicy
+# Step-oobePackageManagement
+# Step-oobeTrustPSGallery
 Step-oobeSetDisplay
 Step-oobeSetRegionLanguage
 Step-oobeSetDateTime
-Step-oobeRegisterAutopilot
+# Step-oobeRegisterAutopilot
 Step-EmbeddedProductKey
-Step-oobeRemoveAppxPackage
+# Step-oobeRemoveAppxPackage
 # Step-oobeAddCapability
 # Step-oobeUpdateDrivers
 # Step-oobeUpdateWindows
