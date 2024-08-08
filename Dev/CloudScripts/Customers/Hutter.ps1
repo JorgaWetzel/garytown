5d1c69cf32419a563a71c10bd75821b21ed49957
JorgaWetzel <51820881+JorgaWetzel@users.noreply.github.com>
2024-08-08 08:48:41 +0200

    Update Hutter.ps1

--- Dev/CloudScripts/Customers/Hutter.ps1
+++ Dev/CloudScripts/Customers/Hutter.ps1
@@ -183,7 +183,8 @@ Invoke-RestMethod https://raw.githubusercontent.com/JorgaWetzel/garytown/master/
 $OOBECMD = @'
 @echo off
 start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\OSDCloud\Scripts\SetupComplete\SetupComplete.ps1 
-reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" /v "execute_provisioning" /t REG_SZ /d "cmd /c powershell.exe -ExecutionPolicy Bypass -File C:\Windows\Setup\Scripts\provisioning.ps1" /f
+start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\provisioning.ps1
+rem reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" /v "execute_provisioning" /t REG_SZ /d "cmd /c powershell.exe -ExecutionPolicy Bypass -File C:\Windows\Setup\Scripts\provisioning.ps1" /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\OOBE" /v "DisablePrivacyExperience" /t REG_DWORD /d 1 /f
 REM RD C:\OSDCloud\OS /S /Q
 REM RD C:\Drivers /S /Q
