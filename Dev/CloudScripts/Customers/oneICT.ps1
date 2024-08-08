bfd41ab249520192d260f6c664b13fdd062909eb
JorgaWetzel <51820881+JorgaWetzel@users.noreply.github.com>
2024-08-08 11:01:47 +0200

    Update oneICT.ps1

--- Dev/CloudScripts/Customers/oneICT.ps1
+++ Dev/CloudScripts/Customers/oneICT.ps1
@@ -82,6 +82,23 @@ if ($DriverPack){
 # Start-OSDCloudGUI
 Start-OSDCloudGUIDev
 
+#Enable HPIA | Update HP BIOS | Update HP TPM
+ 
+if (Test-HPIASupport){
+    #$Global:MyOSDCloud.DevMode = [bool]$True
+    $Global:MyOSDCloud.HPTPMUpdate = [bool]$True
+    if ($Product -ne '83B2' -or $Model -notmatch "zbook"){$Global:MyOSDCloud.HPIAALL = [bool]$true} #I've had issues with this device and HPIA
+    #{$Global:MyOSDCloud.HPIAALL = [bool]$true}
+    $Global:MyOSDCloud.HPBIOSUpdate = [bool]$true
+    $Global:MyOSDCloud.HPCMSLDriverPackLatest = [bool]$true #In Test 
+    #Set HP BIOS Settings to what I want:
+    iex (irm https://raw.githubusercontent.com/gwblok/garytown/master/OSD/CloudOSD/Manage-HPBiosSettings.ps1)
+    Manage-HPBiosSettings -SetSettings
+}
+
+#write variables to console
+Write-Output $Global:MyOSDCloud
+
 #=======================================================================
 #   Unattend.xml
 #=======================================================================
