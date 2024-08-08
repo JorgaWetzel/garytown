8a732d7b49ec08f3ce656cf1c09e73ef8a033b1c
JorgaWetzel <51820881+JorgaWetzel@users.noreply.github.com>
2024-08-08 11:39:00 +0200

    Update Hutter.ps1

--- Dev/CloudScripts/Customers/Hutter.ps1
+++ Dev/CloudScripts/Customers/Hutter.ps1
@@ -131,6 +131,7 @@ Invoke-RestMethod https://raw.githubusercontent.com/JorgaWetzel/garytown/master/
 $OOBECMD = @'
 @echo off
 start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\OSDCloud\Scripts\SetupComplete\SetupComplete.ps1 
+Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/JorgaWetzel/garytown/master/Dev/CloudScripts/Set-KeyboardLanguage.ps1
 rem start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\provisioning.ps1
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" /v "execute_provisioning" /t REG_SZ /d "cmd /c powershell.exe -ExecutionPolicy Bypass -File C:\Windows\Setup\Scripts\provisioning.ps1" /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\OOBE" /v "DisablePrivacyExperience" /t REG_DWORD /d 1 /f
