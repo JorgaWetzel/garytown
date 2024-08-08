6a548b04f54a1d97b480a86c46554312aa77522b
JorgaWetzel <51820881+JorgaWetzel@users.noreply.github.com>
2024-08-08 17:17:37 +0200

    Update PostActionTask2.ps1

--- Dev/CloudScripts/PostActionTask2.ps1
+++ Dev/CloudScripts/PostActionTask2.ps1
@@ -16,51 +16,109 @@ Register-ScheduledTask $ScheduledTaskName -InputObject $task -User SYSTEM
 # Script That Runs:
 $PostActionScript = @'
 
-$RegistryPath = "HKLM:\SOFTWARE\OSDCloud"
-$ScheduledTaskName = 'OSDCloudPostAction'
-
-# Get Current Run, Cleanup if = 5
-[int] $CurrentRun = Get-ItemPropertyValue -Path $RegistryPath -Name 'TriggerPostActions'
-if ($CurrentRun -ge 5){Unregister-ScheduledTask -TaskName $ScheduledTaskName -confirm:$false -ErrorAction SilentlyContinue}
-    
-# Update Post Actions Count
-$UpdateCountTo = $CurrentRun + 1
-New-ItemProperty -Path $RegistryPath -Name "TriggerPostActions" -PropertyType dword -Value $UpdateCountTo -force | Out-Null
-
-# Import Functions from GitHub
-iex (irm functions.garytown.com)
-
-# Update TimeZone 
-Set-TimeZoneFromIP
-
-# Trigger Store Updates
-Invoke-UpdateScanMethodMSStore
-
-# Enable Microsoft Other Updates:
-(New-Object -com "Microsoft.Update.ServiceManager").AddService2("7971f918-a847-4430-9279-4a52d1efe18d",7,"")
-
-# Enable "Notify me when a restart is required to finish updating"
-New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings -Name RestartNotificationsAllowed2 -PropertyType dword -Value 1
-
-# Wait and retrigger Store Updates
-Start-Sleep -Seconds 100
-Invoke-UpdateScanMethodMSStore
-
-if ($CurrentRun -eq 3){
-    if (Test-HPIASupport -eq $true){
-        Run-HPIA -Category All -Action Install -NoninteractiveMode
+# wait for network
+$ProgressPreference_bk = $ProgressPreference
+$ProgressPreference = 'SilentlyContinue'
+do {
+    $ping = Test-NetConnection '8.8.8.8' -InformationLevel Quiet
+    if (!$ping) {
+        cls
+        'Warte auf die Internetverbindung' | Out-Host
+        sleep -s 5
     }
+} while (!$ping)
+$ProgressPreference = $ProgressPreference_bk
+
+$Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-Provisioning.log"
+$null = Start-Transcript -Path (Join-Path "C:\OSDCloud\Logs" $Transcript) -ErrorAction Ignore
+
+Write-Host -ForegroundColor Green "[+] $ScriptName $ScriptVersion ($WindowsPhase Phase)"
+Invoke-Expression -Command (Invoke-RestMethod -Uri functions.osdcloud.com)
+
+osdcloud-SetExecutionPolicy
+osdcloud-SetPowerShellProfile
+osdcloud-InstallPackageManagement
+osdcloud-TrustPSGallery
+osdcloud-InstallPowerShellModule -Name Pester
+osdcloud-InstallPowerShellModule -Name PSReadLine
+# powershell Invoke-Expression -Command (Invoke-RestMethod -Uri pwsh.live)
+# osdcloud-InstallWinGet
+if (Get-Command 'WinGet' -ErrorAction SilentlyContinue) {
+    # Write-Host -ForegroundColor Green '[+] winget upgrade --all --accept-source-agreements --accept-package-agreements'
+    # Write-Host -ForegroundColor Green '[+] winget install company portal (unternehmenbsportal)'
+    # winget install --id "9WZDNCRFJ3PZ" --exact --source msstore --accept-package-agreements --accept-source-agreements
+    # winget upgrade --all --accept-source-agreements --accept-package-agreements
+    # $command = "winget install --id `"9WZDNCRFJ3PZ`" --exact --source msstore --accept-package-agreements --accept-source-agreements"
+    # $runOncePath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce"
+    # $taskName = "InstallAppAtLogon"
+    # Set-ItemProperty -Path $runOncePath -Name $taskName -Value "cmd.exe /c powershell -Command $command" -Force
 }
 
-if (($CurrentRun -ge 2) -and ($CurrentRun -lt 5)){
-    Start-Process shutdown -ArgumentList "/r /t 120 /c ""In 2 Minutes - Currently Performing Initial Setup Modifications - Reboot $CurrentRun of 5""  /f /d p:4:1"
+# osdcloud-InstallPwsh
+# Write-Host -ForegroundColor Green "[+] pwsh.osdcloud.com Complete"
+# osdcloud-UpdateDefenderStack
+# osdcloud-NetFX
+# osdcloud-HPIAExecute
+
+$usb_drive_name = 'USB Drive'
+
+$provisioning = [System.IO.DirectoryInfo]"$($env:ProgramData)\provisioning"
+$urls = @(
+    "https://raw.githubusercontent.com/JorgaWetzel/garytown/master/Dev/CloudScripts/configure_brave.ps1",
+    "https://raw.githubusercontent.com/JorgaWetzel/garytown/master/Dev/CloudScripts/configure_chrome.ps1",
+    "https://raw.githubusercontent.com/JorgaWetzel/garytown/master/Dev/CloudScripts/configure_edge.ps1",
+    "https://raw.githubusercontent.com/JorgaWetzel/garytown/master/Dev/CloudScripts/configure_firefox.ps1"
+)
+
+# Stelle sicher, dass das Verzeichnis existiert
+if (-not (Test-Path $provisioning)) {
+    New-Item -ItemType Directory -Path $provisioning -Force
 }
 
-if ($CurrentRun -ge 5){
-    Start-Process shutdown -ArgumentList "/s /t 120 /c ""In 2 Minutes - !! -- Shutting down PC to signal process completed -- !!""  /f /d p:4:1"
+# Herunterladen und Ausführen der Konfigurationsskripte
+foreach ($url in $urls) {
+    $scriptName = [System.IO.Path]::GetFileName($url)
+    $scriptPath = Join-Path -Path $provisioning -ChildPath $scriptName
+    
+    # Herunterladen, wenn das Skript noch nicht existiert
+    if (-not (Test-Path $scriptPath)) {
+        Invoke-WebRequest -Uri $url -OutFile $scriptPath
+    }
+    
+    # Ausführen des Skripts
+    . $scriptPath
 }
 
-'@
+# Chocolatey software installation
+choco.exe install office365business --params "'/exclude:Access Groove Lync Publisher /language:de-DE /eula:FALSE'" -y --no-progress --ignore-checksums
 
-$PostActionScript | Out-File -FilePath $ScriptPath -Force
+$packages = "TeamViewer","adobereader","microsoft-teams-new-bootstrapper","googlechrome","7zip.install","firefox","vlc","jre8","powertoys","onedrive","Pdf24","vcredist140","zoom","notepadplusplus.install","onenote","onedrive"
+$packages | %{
+    choco install $_ -y --no-progress --ignore-checksums
+}
 
+# Version=1
+
+# syspin ["file"] #### or syspin ["file"] "commandstring"
+# 5386  : Pin to taskbar
+# 5387  : Unpin from taskbar
+# 51201 : Pin to start
+# 51394 : Unpin to start
+# Download syspin.exe
+$url32 = 'http://www.technosys.net/download.aspx?file=syspin.exe'
+$output = "$env:TEMP\syspin.exe"
+Invoke-WebRequest -Uri $url32 -OutFile $output
+
+# Pin the shortcut to the taskbar
+# & "$env:TEMP\syspin.exe" "C:\Program Files\Google\Chrome\Application\chrome.exe" 5386
+& "$env:TEMP\syspin.exe" "C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE" 5386
+& "$env:TEMP\syspin.exe" "C:\Program Files\Microsoft Office\root\Office16\WINWORD.EXE" 5386
+& "$env:TEMP\syspin.exe" "C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE" 5386
+& "$env:TEMP\syspin.exe" "C:\Program Files\Microsoft Office\root\Office16\POWERPNT.EXE" 5386
+& "$env:TEMP\syspin.exe" "C:\Program Files\Google\Chrome\Application\chrome.exe" 5386
+
+& "$env:TEMP\syspin.exe" "$env:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs\Word.lnk" 51201
+& "$env:TEMP\syspin.exe" "$env:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs\Excel.lnk" 51201
+& "$env:TEMP\syspin.exe" "$env:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs\Firefox.lnk" 51201
+
+function Remove
