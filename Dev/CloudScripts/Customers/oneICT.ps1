0283e9ba5e9599a55651af9c1217174477f28d4b
JorgaWetzel <51820881+JorgaWetzel@users.noreply.github.com>
2024-08-21 16:37:29 +0200

    Update oneICT.ps1

--- Dev/CloudScripts/Customers/oneICT.ps1
+++ Dev/CloudScripts/Customers/oneICT.ps1
@@ -115,13 +115,25 @@ $UnattendXml = @'
             </RunSynchronous>
         </component>
     </settings>
+
     <settings pass="oobeSystem">
         <component name="Microsoft-Windows-Deployment" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
             <Reseal>
                 <Mode>Audit</Mode>
             </Reseal>
         </component>
+        <component name="Microsoft-Windows-International-Core" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            <InputLocale>de-CH</InputLocale>
+            <SystemLocale>de-CH</SystemLocale>
+            <UILanguage>de-CH</UILanguage>
+            <UILanguageFallback>en-US</UILanguageFallback>
+            <UserLocale>de-CH</UserLocale>
+        </component>
+        <component name="Microsoft-Windows-Shell-Setup" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            <InputLocale>de-CH</InputLocale>
+        </component>
     </settings>
+
     <settings pass="auditUser">
         <component name="Microsoft-Windows-Deployment" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
             <RunSynchronous>
