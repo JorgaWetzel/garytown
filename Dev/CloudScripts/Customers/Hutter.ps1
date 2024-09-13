41b6d45e989a904655bb0de671d857310256ee18
JorgaWetzel <51820881+JorgaWetzel@users.noreply.github.com>
2024-09-13 09:44:19 +0200

    Update Hutter.ps1

--- Dev/CloudScripts/Customers/Hutter.ps1
+++ Dev/CloudScripts/Customers/Hutter.ps1
@@ -146,7 +146,7 @@ $OOBEDeployJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.OOBEDeplo
 #================================================
 Write-Host -ForegroundColor Green "Define Computername:"
 $Serial = Get-WmiObject Win32_bios | Select-Object -ExpandProperty SerialNumber
-$TargetComputername = $Serial.Substring(4,3)
+$TargetComputername = $Serial.Substring(4,8)
 
 $AssignedComputerName = "Hutter-$TargetComputername"
 Write-Host -ForegroundColor Red $AssignedComputerName
