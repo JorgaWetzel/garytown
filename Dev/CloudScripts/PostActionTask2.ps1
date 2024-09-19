e07964e0649fac7016f1b94766d2b4e41875f2a9
JorgaWetzel <51820881+JorgaWetzel@users.noreply.github.com>
2024-09-19 11:28:58 +0200

    Update PostActionTask2.ps1

--- Dev/CloudScripts/PostActionTask2.ps1
+++ Dev/CloudScripts/PostActionTask2.ps1
@@ -318,15 +318,18 @@ try {
 $parentFolder = "C:\Program Files\oneICT\EndpointManager"
 $folder1 = "$parentFolder\Data"
 $folder2 = "$parentFolder\Log"
+$folder3 = "C:\service"
 
 # Create the folders if they do not exist
-New-Item -Path $folder1 -ItemType Directory -Force
-New-Item -Path $folder2 -ItemType Directory -Force
+New-Item -Path $folder1 -ItemType Directory -Force | Out-Null
+New-Item -Path $folder2 -ItemType Directory -Force | Out-Null
+New-Item -Path $folder3 -ItemType Directory -Force | Out-Null
 
 # Define the permission rule for Everyone
 $aclParent = Get-Acl $parentFolder
 $acl1 = Get-Acl $folder1
 $acl2 = Get-Acl $folder2
+$acl3 = Get-Acl $folder3
 
 $everyone = [System.Security.Principal.NTAccount]"Jeder"
 $rights = [System.Security.AccessControl.FileSystemRights]::FullControl
@@ -338,11 +341,13 @@ $accessRule = New-Object System.Security.AccessControl.FileSystemAccessRule($eve
 $aclParent.AddAccessRule($accessRule)
 $acl1.AddAccessRule($accessRule)
 $acl2.AddAccessRule($accessRule)
+$acl3.AddAccessRule($accessRule)
 
 # Apply the updated ACL to the parent folder and subfolders
 Set-Acl -Path $parentFolder -AclObject $aclParent
 Set-Acl -Path $folder1 -AclObject $acl1
 Set-Acl -Path $folder2 -AclObject $acl2
+Set-Acl -Path $folder3 -AclObject $acl3
 
 "powercfg /x -monitor-timeout-ac 0",
 "powercfg /x -standby-timeout-ac 0",
