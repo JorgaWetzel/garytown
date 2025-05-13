0f1a9b1fcdcbbe51ab95a29c5d162cd9acbf4854
JorgaWetzel <51820881+JorgaWetzel@users.noreply.github.com>
2025-05-13 18:28:27 +0200

    Update

--- Dev/CloudScripts/Customers/VarioTradeMUI - 6.ps1
+++ Dev/CloudScripts/Customers/VarioTradeMUI - 6.ps1
@@ -66,7 +66,7 @@ $Global:MyOSDCloud = @{
 
 
 # -------------------------------------------------
-# HP section one compact block
+# HP section – one compact block
 # -------------------------------------------------
 if ($cs.Manufacturer -match 'HP') {
 
@@ -95,8 +95,46 @@ if ($cs.Manufacturer -match 'HP') {
 Write-Output $Global:MyOSDCloud
 
 
-# --- Deployment ---------------------------------------------
+# --- Deployment ausführen ---------------------------------------------
 Invoke-OSDCloud
+
+
+#region OOBE Tasks
+#================================================
+Write-SectionHeader "[PostOS] OOBE CMD Command Line"
+#================================================
+Write-DarkGrayHost "Downloading Scripts for OOBE and specialize phase"
+
+#Invoke-RestMethod http://autopilot.osdcloud.ch | Out-File -FilePath 'C:\Windows\Setup\scripts\autopilot.ps1' -Encoding ascii -Force
+# Invoke-RestMethod http://oobe.osdcloud.ch | Out-File -FilePath 'C:\Windows\Setup\scripts\oobe.ps1' -Encoding ascii -Force
+Invoke-RestMethod http://cleanup.osdcloud.ch | Out-File -FilePath 'C:\Windows\Setup\scripts\cleanup.ps1' -Encoding ascii -Force
+#Invoke-RestMethod http://osdgather.osdcloud.ch | Out-File -FilePath 'C:\Windows\Setup\scripts\osdgather.ps1' -Encoding ascii -Force
+
+$OOBEcmdTasks = @'
+@echo off
+
+REM Wait for Network 10 seconds
+REM ping 127.0.0.1 -n 10 -w 1  >NUL 2>&1
+
+REM Execute OOBE Tasks
+REM start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\oobe.ps1
+
+REM Execute OSD Gather Script
+REM start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\osdgather.ps1
+
+REM Execute Cleanup Script
+start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\cleanup.ps1
+
+REM Below a PS session for debug and testing in system context, # when not needed 
+REM start /wait powershell.exe -NoL -ExecutionPolicy Bypass
+
+exit 
+'@
+$OOBEcmdTasks | Out-File -FilePath 'C:\Windows\Setup\scripts\oobe.cmd' -Encoding ascii -Force
+
+Write-DarkGrayHost "Copying PFX file"
+Copy-Item X:\OSDCloud\Config\Scripts C:\OSDCloud\ -Recurse -Force
+#endregion
 
 Initialize-OSDCloudStartnetUpdate
 Restart-Computer -Force
