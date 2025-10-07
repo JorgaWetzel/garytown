7ec7b145ad36b478bf6e8fbf8cf334f5449c7ed1
JorgaWetzel <51820881+JorgaWetzel@users.noreply.github.com>
2025-10-07 10:45:00 +0200

    Update VT_AIO.ps1

--- Dev/CloudScripts/Testing/VT_AIO.ps1
+++ Dev/CloudScripts/Testing/VT_AIO.ps1
@@ -3,11 +3,13 @@ try { Add-Type -AssemblyName Microsoft.VisualBasic -ErrorAction Stop } catch {}
 $to = $null
 $to = [Microsoft.VisualBasic.Interaction]::InputBox("E-Mail-Adresse des Empfängers:","KillDisk Report","")
 
-# KillDisk starten
-& "X:\OSDCloud\Config\Bootdisk\KillDisk.exe" -wa -bm em=1
+# KillDisk starten und warten bis Ende
+$proc = Start-Process 'X:\Bootdisk\KillDisk.exe' -ArgumentList '-wa','-bm','em=1' -PassThru
+$proc.WaitForExit()
 
 # PDFs einsammeln
-$files = Get-ChildItem -Path "X:\OSDCloud\Config\Bootdisk\" -Filter Certificate*.pdf -File | Select-Object -Expand FullName
+$pdfFile = Get-ChildItem -Path "X:\OSDCloud\Config\Bootdisk\" -Filter Certificate*.pdf -File | Select-Object -Expand FullName
+$att=@($pdfFile.FullName); 'X:\OSDCloud\logs\Win32_BaseBoard.txt','X:\OSDCloud\logs\Win32_BIOS.txt','X:\OSDCloud\logs\Win32_ComputerSystem.txt','X:\OSDCloud\logs\Win32_DiskDrive.txt','X:\OSDCloud\logs\Win32_Processor.txt'|%{if(Test-Path $_){$att+=$_}}
 
 # TLS 1.2 für O365 erzwingen
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
@@ -19,5 +21,7 @@ $cred = New-Object System.Management.Automation.PSCredential(
 )
 
 # Mail senden
-Send-MailMessage -From 'service@variotrade.ch' -To $to -Subject 'KillDisk Report' -Body 'Im Anhang.' `
-  -Attachments $files -SmtpServer 'smtp.office365.com' -Port 587 -UseSsl -Credential $cred
+Send-MailMessage -From 'service@variotrade.ch' -To $to -Subject 'Löschzertifikat – Systembereinigung durch VarioTrade' -Body `
+"Das System wurde von VarioTrade erfolgreich gelöscht. 
+Im Anhang finden Sie das Löschzertifikat sowie die relevanten Hardware-Informationen." `
+-Attachments $att -SmtpServer 'smtp.office365.com' -Port 587 -UseSsl -Credential $cred
